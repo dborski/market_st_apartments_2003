c9ef372eb7a61b43736cb91292c901a10473a976
Derek Borski <derek.borski@gmail.com>
2020-03-31 09:18:39 -0600

    Adds renter and add_renter methods

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,10 +1,15 @@
 class Apartment
 
-  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
   def initialize(init_hash)
     @number = init_hash[:number]
     @monthly_rent = init_hash[:monthly_rent]
     @bathrooms = init_hash[:bathrooms]
     @bedrooms = init_hash[:bedrooms]
+    @renter = nil
+  end
+
+  def add_renter(renter)
+    @renter = renter
   end
 end
--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -7,6 +7,7 @@ class ApartmentTest < Minitest::Test
 
   def setup
     @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @renter1 = Renter.new("Jessie")
   end
 
   def test_it_exists
@@ -19,36 +20,17 @@ class ApartmentTest < Minitest::Test
     assert_equal 1, @unit1.bathrooms
     assert_equal 1, @unit1.bedrooms
   end
+
+  def test_can_add_renter
+    assert_equal nil, @unit1.renter
+
+    @unit1.add_renter(@renter1)
+
+    assert_equal @renter1, @unit1.renter
+  end
 end
 
-# ## Iteration 1
-#
-# Use TDD to create a Renter class and an Apartment class that respond to the following interaction pattern:
 
-#
-# require './lib/apartment'
-# # => true
-#
-# # => #<Renter:0x00007a15c93af80...>
-#
-# renter1.name
-# # => "Jessie"
-#
-# unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-# # => #<Apartment:0x00007fb3ecaae7c0...>
-#
-# unit1.number
-# # => "A1"
-#
-# unit1.monthly_rent
-# # => 1200
-#
-# unit1.bathrooms
-# # => 1
-#
-# unit1.bedrooms
-# # => 1
-#
 # unit1.renter
 # # => nil
 #
